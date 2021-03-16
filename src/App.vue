1a37f0c0df2f5c231d148c7cb1c626b84bb48729
waffe_x20 <argueta_eduardo@yahoo.de>
2021-03-15 22:31:01 -0400

    ImNotScared

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,50 @@
 <template>
     <div id="haupt-app" class="container">
         <h4>{{ Titel }}</h4>
-        <font-awesome-icon icon="plus" class="mr-2" /> <button class="btn btn-success">mehr Mühe geben</button> 
-        <br><hr>
-        <appointment-list :appointments="appointments"></appointment-list>
+        <font-awesome-icon icon="plus" class="mr-2" />
+        <button class="btn btn-success">mehr Mühe geben</button> <br />
+        <hr />
+        <div class="row justify-content-center">
+            <appointment-list
+                :appointments="appointments"
+                @remove="removeItem"
+            />
+        </div>
     </div>
 </template>
 
 <script>
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import AppointmentList from "./components/AppointmentList"
+import AppointmentList from "./components/AppointmentList";
 import axios from "axios";
+//import func from 'vue-editor-bridge';
+import _ from "lodash";
 
 export default {
     name: "hauptApp",
     data: function() {
         return {
             Titel: "New Way Home",
-            appointments: []
+            appointments: [],
+            aptIndex: 0,
         };
     },
     components: {
-        FontAwesomeIcon,
-        AppointmentList 
+        AppointmentList,
     },
-    mounted(){     
-      axios
-      .get("./data/appointments.json")
-      .then(response => (this.appointments = response.data));
-    }
+    mounted() {
+        axios.get("./data/appointments.json").then(
+            response =>
+                (this.appointments = response.data.map(item => {
+                    item.aptId = this.aptIndex;
+                    this.aptIndex++;
+                    return item;
+                }))
+        );
+    },
+    methods: {
+        removeItem: function(apt) {
+            this.appointments = _.without(this.appointments, apt);
+        },
+    },
 };
 </script>
